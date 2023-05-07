bd8316a4c1e91bf70ef8774a0fc22c8e2cf8aa1c
Lexpiper <lexpiperfx@gmail.com>
2023-05-07 11:55:52 +0100

    adds Award components

--- ftmo/src/components/Awards.jsx
+++ ftmo/src/components/Awards.jsx
@@ -1,8 +1,12 @@
-
+import { imgLinks } from "../constants/styles";
 const Awards = () => {
   return (
-    <div>Awards</div>
-  )
-}
+    <div className="bg-[#f4f4f4] grid grid-cols-6 gap-4 align-baseline pt-[4.6875rem] pb-[4.6875rem]">
+      {imgLinks.map((lin) => (
+        <img key={lin.id} src={lin.link} className="max-w-full h-auto" alt="img" />
+      ))}
+    </div>
+  );
+};
 
-export default Awards
+export default Awards;
