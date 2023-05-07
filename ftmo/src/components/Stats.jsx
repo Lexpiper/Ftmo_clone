5c75499b2c75c7d04f1a11e9b03b0761bd49e418
Lexpiper <lexpiperfx@gmail.com>
2023-05-07 11:59:56 +0100

    adds stat components

--- ftmo/src/components/Stats.jsx
+++ ftmo/src/components/Stats.jsx
@@ -1,12 +1,16 @@
+import { statlinks } from "../constants/styles";
 
 const stat = () => {
   return (
-    <div className="bg-[#262729]">
-      <div className="flex items-center ">
-        
-      </div>
+    <div className="flex flex-wrap justify-center py-8 bg-[#262729] ">
+      {statlinks.map((stat) => (
+        <div key={stat.id} className="w-full h-fu md:w-1/2 lg:w-1/4 p-4 text-center  font-poppins font-normal">
+          <h2 className="tracking-wider text-5xl font-bold text-[#0781fe] pb-4">{stat.figure}</h2>
+          <span className="text-sm  max-w-[70%] font-medium text-center text-white">{stat.text}</span>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default stat
+export default stat;
