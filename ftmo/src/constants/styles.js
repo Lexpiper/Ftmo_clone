f92873659da90bb6b61fe42b7616663adb933023
Lexpiper <lexpiperfx@gmail.com>
2023-05-07 12:08:02 +0100

    Refactor styles constants for better organization and readability.

--- ftmo/src/constants/styles.js
+++ ftmo/src/constants/styles.js
@@ -30,5 +30,51 @@ export const navLinks = [
 ];
 
 export const statlinks = [
-  
+  {
+    id: 1,
+    figure: "$130M+",
+    text: "paid out to FTMO traders",
+  },
+  {
+    id: 2,
+    figure: "180+",
+    text: "No. of countries with traders registered at FTMO",
+  },
+  {
+    id: 3,
+    figure: "16M+",
+    text: "No. of trades opened every month",
+  },
+  {
+    id: 4,
+    figure: "8h",
+    text: "Avg payout processing time",
+  },
+];
+
+export const imgLinks = [
+  {
+    id: 1,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/ey.svg",
+  },
+  {
+    id: 2,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/forbes.svg",
+  },
+  {
+    id: 3,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2022.svg",
+  },
+  {
+    id: 4,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2021.svg",
+  },
+  {
+    id: 5,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2020.svg",
+  },
+  {
+    id: 6,
+    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2019.svg",
+  },
 ];
