2e636ac55bf681b13df5392fc735bb82b19c9c9c
Lexpiper <lexpiperfx@gmail.com>
2023-05-07 12:09:45 +0100

    add info component

---
+++ ftmo/src/components/Info.jsx
@@ -0,0 +1,12 @@
+const Info = () => {
+  return (
+    <div className="bg-[#f4f4f4] pb-[4.6875rem] pt-[4.6875rem] ">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime vitae
+      velit impedit qui, doloremque accusantium voluptatum fuga ipsa quis
+      pariatur quo, dolore odit reprehenderit asperiores harum deleniti.
+      Exercitationem, dignissimos commodi?
+    </div>
+  );
+};
+
+export default Info;
