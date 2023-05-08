e809e2a77974b62a054b038cd2552804f2604b33
Lexpiper <lexpiperfx@gmail.com>
2023-05-08 09:31:40 +0100

    add global  tailwind styling

--- ftmo/tailwind.config.js
+++ ftmo/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0781fe",
+        secondary: "#00f6ff",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
