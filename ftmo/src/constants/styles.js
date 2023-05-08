5e3be2b129aabed4e3e652491ec228bcba003fb6
Lexpiper <lexpiperfx@gmail.com>
2023-05-08 19:08:02 +0100

    update constat objects

--- ftmo/src/constants/styles.js
+++ ftmo/src/constants/styles.js
@@ -1,80 +1,131 @@
 export default {
-  navLinks : [
-  {
-    id: "1",
-    title: "How it works ^",
-  },
-  {
-    id: "2",
-    title: "FAQ",
-  },
-  {
-    id: "3",
-    title: "Testimonial",
-  },
-  {
-    id: "4",
-    title: "Trading",
-  },
-  {
-    id: "5",
-    title: "contact",
-  },
-  {
-    id: "6",
-    title: "Careers",
-  },
-  {
-    id: "7",
-    title: "Client Area",
-  },
-],
-statlinks : [
-  {
-    id: 1,
-    figure: "$130M+",
-    text: "paid out to FTMO traders",
-  },
-  {
-    id: 2,
-    figure: "180+",
-    text: "No. of countries with traders registered at FTMO",
-  },
-  {
-    id: 3,
-    figure: "16M+",
-    text: "No. of trades opened every month",
-  },
-  {
-    id: 4,
-    figure: "8h",
-    text: "Avg payout processing time",
-  },
-],
-imgLinks : [
-  {
-    id: 1,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/ey.svg",
-  },
-  {
-    id: 2,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/forbes.svg",
-  },
-  {
-    id: 3,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2022.svg",
-  },
-  {
-    id: 4,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2021.svg",
-  },
-  {
-    id: 5,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2020.svg",
-  },
-  {
-    id: 6,
-    link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2019.svg",
-  },
-]
+  navLinks: [
+    {
+      id: "1",
+      title: "How it works ^",
+    },
+    {
+      id: "2",
+      title: "FAQ",
+    },
+    {
+      id: "3",
+      title: "Testimonial",
+    },
+    {
+      id: "4",
+      title: "Trading",
+    },
+    {
+      id: "5",
+      title: "contact",
+    },
+    {
+      id: "6",
+      title: "Careers",
+    },
+    {
+      id: "7",
+      title: "Client Area",
+    },
+  ],
+  statlinks: [
+    {
+      id: 1,
+      figure: "$130M+",
+      text: "paid out to FTMO traders",
+    },
+    {
+      id: 2,
+      figure: "180+",
+      text: "No. of countries with traders registered at FTMO",
+    },
+    {
+      id: 3,
+      figure: "16M+",
+      text: "No. of trades opened every month",
+    },
+    {
+      id: 4,
+      figure: "8h",
+      text: "Avg payout processing time",
+    },
+  ],
+  imgLinks: [
+    {
+      id: 1,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/ey.svg",
+    },
+    {
+      id: 2,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/forbes.svg",
+    },
+    {
+      id: 3,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2022.svg",
+    },
+    {
+      id: 4,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2021.svg",
+    },
+    {
+      id: 5,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2020.svg",
+    },
+    {
+      id: 6,
+      link: "https://ftmo.com/wp-content/themes/ftmo-com/public/images/svg/awards/deloitte_2019.svg",
+    },
+  ],
+  Eval: [
+    {
+      id: 1,
+      No: "01.",
+      title: "FTMO Challenge",
+      text: "An FTMO Challenge is the first step of the Evaluation Process. You need to succeed here to advance into the Verification stage. Prove your trading skills and discipline in observing the Trading Objectives.",
+    },
+    {
+      id: 2,
+      No: "02.",
+      title: "Verification",
+      text: "Verification is the second and the last step towards becoming an FTMO Trader. Once you pass the Verification stage and your results are verified, you will be offered to trade for our Proprietary Trading Firm.",
+    },
+  ],
+  currency: [
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/usd-round-flag.417cef31.png",
+      sign: "USD",
+    },
+    {
+      id: 2,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/gbp-round-flag.292cd401.png",
+      sign: "GBP",
+    },
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/eur-round-flag.3ee46dd1.png",
+      sign: "EUR",
+    },
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/czk-round-flag.4569f892.png",
+      sign: "CZK",
+    },
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/cad-round-flag.8e4db08c.png",
+      sign: "CAD",
+    },
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/aud-round-flag.1507fce3.png",
+      sign: "AUD",
+    },
+    {
+      id: 1,
+      img: "https://ftmo.com/wp-content/plugins/trading-objectives/build/images/chf-round-flag.adfe7382.png",
+      sign: "CHF",
+    },
+  ],
 };
