2990db8fcbfa49fc8411d816bf9e0de0da3ef933
Lexpiper <lexpiperfx@gmail.com>
2023-05-08 19:02:30 +0100

    add new styling config

--- ftmo/tailwind.config.js
+++ ftmo/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         primary: "#0781fe",
         secondary: "#00f6ff",
+        alt: "#00C7B4",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
