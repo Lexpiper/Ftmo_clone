4a3465f144232bdcba89dfb097810a095e21f1c8
Lexpiper <lexpiperfx@gmail.com>
2023-06-02 09:01:21 +0100

    modify the data styles

--- ftmo/src/components/Awards.jsx
+++ ftmo/src/components/Awards.jsx
@@ -1,8 +1,8 @@
-import styles from "../constants/styles";
+import data from "../constants/data";
 const Awards = () => {
   return (
     <div className="bg-[#f4f4f4] grid grid-cols-6 gap-4 align-baseline pt-[4.6875rem] pb-[4.6875rem]">
-      {styles.imgLinks.map((lin) => (
+      {data.imgLinks.map((lin) => (
         <img key={lin.id} src={lin.link} className="max-w-full h-auto" alt="img" />
       ))}
     </div>
