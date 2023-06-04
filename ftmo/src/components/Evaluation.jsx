1aa0837a4cbd644b75588b956e8043d78cdf2498
Lexpiper <lexpiperfx@gmail.com>
2023-06-04 17:18:27 +0100

    modify reponsivness

--- ftmo/src/components/Evaluation.jsx
+++ ftmo/src/components/Evaluation.jsx
@@ -3,26 +3,19 @@ import data from "../constants/data";
 
 const Evaluation = () => {
   return (
-    <div className="bg-[#f4f4f4] w-full h-full mt-20 ">
+    <div className="bg-[#f4f4f4] w-full mt-20">
       <h3 className="text-center mb-10 bg-[#f4f4f4] w-full text-[2.0rem] text-primary font-poppins font-semibold tracking-wider">
         EVALUATION PROCESS
       </h3>
-      <div className="grid grid-cols-3 gap-4 bg-[#f4f4f4]">
-        <div className="col-span-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 bg-[#f4f4f4]">
+        <div className="sm:col-span-2">
           <div className="flex flex-col items-center">
             <span>This is where we evaluate your trading</span>
-            {/* <hr className="w-[70%] h-2 text-red-600 border" /> */}
-            {/* <div className="flex items-center">
-            <div className="w-full border-t border-gray-400" />
-            <div className="bg-gray-400 rounded-full h-4 w-4 ml-2 sm:ml-4 lg:ml-8" />
-            <div className="bg-gray-400 rounded-full h-4 w-4 mr-2 sm:mr-4 lg:mr-8" />
-            <div className="w-full border-t border-gray-400" />
-		</div> */}
-            <div className="flex items-center w-full">
+            <div className="flex flex-col sm:flex-row items-center w-full">
               {data.Eval.map((eva) => (
                 <div
                   key={eva.id}
-                  className="flex flex-col w-full m-[6.5%] min-h-[309px] p-[25px] rounded-[20px] text-left align-top bg-white"
+                  className="flex flex-col w-full sm:w-1/2 m-3 sm:m-[6.5%] min-h-[309px] p-[25px] rounded-[20px] text-left align-top bg-white"
                 >
                   <div className="text-[#c6c6c6] text-[2.5rem] font-poppins leading-[50px] font-semibold">
                     {eva.No}
@@ -38,7 +31,7 @@ const Evaluation = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-1 w-full inline-block relative mb-0 min-h-[309px] p-[25px] rounded-[20px] text-left align-top bg-white">
+        <div className="w-full inline-block relative mb-0 min-h-[309px] p-[25px] rounded-[20px] text-left align-top bg-white">
           <Button
             padding={"px-[20px] py-[10px]"}
             bg={"bg-[#00C7B4]"}
