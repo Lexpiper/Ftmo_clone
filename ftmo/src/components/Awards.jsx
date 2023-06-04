76ad0f163bc62a9d2846f1cc3faf80d53047da34
Lexpiper <lexpiperfx@gmail.com>
2023-06-04 17:17:00 +0100

    make component responsivw

--- ftmo/src/components/Awards.jsx
+++ ftmo/src/components/Awards.jsx
@@ -1,7 +1,8 @@
 import data from "../constants/data";
+
 const Awards = () => {
   return (
-    <div className="bg-[#f4f4f4] grid grid-cols-6 gap-4 align-baseline pt-[4.6875rem] pb-[4.6875rem]">
+    <div className="bg-[#f4f4f4] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 py-16">
       {data.imgLinks.map((lin) => (
         <img key={lin.id} src={lin.link} className="max-w-full h-auto" alt="img" />
       ))}
